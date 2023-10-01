acae0e24a73b578d4acc0feb020151f4605b0368
syg0629 <syg0629@hanmail.net>
2023-10-02 00:00:03 +0900

    코드 리팩토링 1개 및 새로푼 문제 2개

--- programmers/lv.1/230920-올바른괄호.js
+++ programmers/lv.1/230920-올바른괄호.js
@@ -1,4 +1,5 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12909
+//시간 초과
 
 // function solution(s) {
 //   for (let i = s.length; i > 1; i -= 2) {
@@ -6,8 +7,6 @@
 //   }
 //   return s === "" ? true : false;
 // }
-
-//시간 초과
 
 // function solution(s) {
 //   for (let i = s.length; i > 0; i -= 2) {
@@ -18,8 +17,6 @@
 //   }
 //   return s === "" ? true : false;
 // }
-
-//시간 초과
 
 // function solution(s) {
 //   for (let i = s.length; i > 0; i -= 2) {
@@ -31,11 +28,29 @@
 //   }
 //     return false;
 // }
-//시간 초과
+
+// function solution(s) {
+//   if (s.charAt(0) === ")") return false;
+//   let arr = [];
+//   s.split("").forEach((n) => (n === "(" ? arr.push("(") : arr.pop()));
+//   return arr.length === 0 ? true : false;
+// }
 
 function solution(s) {
-  if (s.charAt(0) === ")") return false;
-  let arr = [];
-  s.split("").forEach((n) => (n === "(" ? arr.push("(") : arr.pop()));
-  return arr.length === 0 ? true : false;
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i) === "(") {
+      count++;
+    } else {
+      count--;
+      if (count < 0) return false;
+    }
+  }
+  return count === 0;
 }
+
+console.log(solution("()()")); //true
+console.log(solution("(())()")); //true
+console.log(solution(")()(")); //false
+console.log(solution("(()(")); //false
+console.log(solution("())(()")); //false
