8f5e605781078c324da68a53f80fbaf29ed39720
syg0629 <syg0629@hanmail.net>
2023-10-10 22:08:15 +0900

    코드 리팩토링

--- programmers/lv.1/230917-가운데글자가져오기.ts
+++ programmers/lv.1/230917-가운데글자가져오기.ts
@@ -1,13 +1,19 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12903
 export {};
 
+// function solution(s: string): string {
+//   const halfLength = s.length / 2;
+//   return s.length % 2 === 0
+//     ? s.split("")[halfLength - 1] + s.split("")[halfLength]
+//     : s.split("")[Math.floor(halfLength)];
+// }
+
 function solution(s: string): string {
-  const halfLength = s.length / 2;
+  const mid: number = Math.floor(s.length / 2);
   return s.length % 2 === 0
-    ? s.split("")[halfLength - 1] + s.split("")[halfLength]
-    : s.split("")[Math.floor(halfLength)];
+    ? s.substring(mid - 1, mid + 1)
+    : s.substring(mid, mid + 1);
 }
 
-// 리팩토링 다시 시도해 볼 것
 console.log(solution("abcde")); //"c"
 console.log(solution("qwer")); //"we"
--- programmers/lv.1/230920-같은숫자는싫어.ts
+++ programmers/lv.1/230920-같은숫자는싫어.ts
@@ -1,10 +1,14 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12906
 export {};
 
+// function solution(arr: number[]): number[] {
+//   let answer: number[] = [];
+//   arr.forEach((_, i) => (arr[i] !== arr[i + 1] ? answer.push(arr[i]) : ""));
+//   return answer;
+// }
+
 function solution(arr: number[]): number[] {
-  let answer: number[] = [];
-  arr.forEach((_, i) => (arr[i] !== arr[i + 1] ? answer.push(arr[i]) : ""));
-  return answer;
+  return arr.filter((_, i: number) => arr[i] !== arr[i + 1]);
 }
 
 console.log(solution([1, 1, 3, 3, 0, 1, 1])); //[1,3,0,1]
