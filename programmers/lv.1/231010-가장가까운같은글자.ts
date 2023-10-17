02d62c47743103301c9b1a9fd381b4c9f2822a27
syg0629 <syg0629@hanmail.net>
2023-10-17 23:58:12 +0900

    코드 리팩토링(for문 map으로 수정 및 변수명 변경)

--- programmers/lv.1/231010-가장가까운같은글자.ts
+++ programmers/lv.1/231010-가장가까운같은글자.ts
@@ -1,46 +1,17 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/142086
 export {};
 
-// function solution(s: string): number[] {
-//   let answer: number[] = [];
-//   let dupCheck: string[] = [];
-//   for (let x of s) {
-//     if (!dupCheck.includes(x)) {
-//       answer.push(-1);
-//     } else {
-//       answer.push(dupCheck.length - dupCheck.lastIndexOf(x));
-//     }
-//     dupCheck.push(x);
-//   }
-//   return answer;
-// }
-
-//타입스크립트는 string 타입의 key를 허용하지 않음
-// function solution(s: string): number[] {
-//   const answer: number[] = [];
-//   const distanceChk = {};
-//   for (const [index, value] of Object.entries(s)) {
-//     if (distanceChk[value] === undefined) {
-//       answer.push(-1);
-//     } else {
-//       answer.push(index - distanceChk[value]);
-//     }
-//     distanceChk[value] = index;
-//   }
-//   return answer;
-// }
-
 function solution(s: string): number[] {
   const answer: number[] = [];
-  const distanceChk: Record<string, number> = {};
-  for (const [index, value] of Object.entries(s)) {
-    if (distanceChk[value] === undefined) {
+  const lastIndices: Record<string, number> = {};
+  s.split("").map((x: string, i: number) => {
+    if (lastIndices[x] === undefined) {
       answer.push(-1);
     } else {
-      answer.push(Number(index) - distanceChk[value]);
+      answer.push(i - lastIndices[x]);
     }
-    distanceChk[value] = Number(index);
-  }
+    lastIndices[x] = i;
+  });
   return answer;
 }
 
