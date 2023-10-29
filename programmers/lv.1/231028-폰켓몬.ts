6de9b73f48945d95c8c36bbf87a191783a204982
syg0629 <syg0629@hanmail.net>
2023-10-30 08:02:13 +0900

    코드 리팩토링(capital case 수정)

--- programmers/lv.1/231028-폰켓몬.ts
+++ programmers/lv.1/231028-폰켓몬.ts
@@ -2,10 +2,10 @@
 export {};
 
 function solution(nums: number[]): number {
-  const Dedup: Set<number> = new Set(nums);
-  const DedupNum: number = Dedup.size;
-  const Selectable: number = nums.length / 2;
-  return DedupNum > Selectable ? Selectable : DedupNum;
+  const dedup: Set<number> = new Set(nums);
+  const dedupNum: number = dedup.size;
+  const selectable: number = nums.length / 2;
+  return dedupNum > selectable ? selectable : dedupNum;
 }
 
 console.log(solution([3, 1, 2, 3])); //2
