a9162856577f59d41db8a723771533856a7eb02c
syg0629 <syg0629@hanmail.net>
2023-11-02 08:32:37 +0900

    코드 리팩토링(정렬없는 풀이)

--- programmers/lv.2/231028-전화번호목록.ts
+++ programmers/lv.2/231028-전화번호목록.ts
@@ -1,29 +1,27 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/42577
 export {};
 
-function solution(phone_book: string[]) {
+function nonDuplicatedPrefix(phoneBook: string[]): boolean {
   const phoneNum: Record<string, boolean> = {};
 
-  for (let i = 1; i < phone_book.length; i++) {
-    const temp: string = phone_book[i];
-    let j = 0;
-
-    for (j = i - 1; j >= 0; j--) {
-      if (temp < phone_book[j]) {
-        phone_book[j + 1] = phone_book[j];
-      } else break;
-    }
-    phone_book[j + 1] = temp;
-  }
-
-  for (const x of phone_book) {
+  for (const x of phoneBook) {
     phoneNum[x] = true;
     for (let i = 1; i < x.length; i++) {
       const cutPhoneNum: string = x.slice(0, i);
-      if (phoneNum[cutPhoneNum]) return false;
+      if (phoneNum[cutPhoneNum]) {
+        return false;
+      }
     }
   }
   return true;
+}
+
+function solution(phone_book: string[]): boolean {
+  if (nonDuplicatedPrefix(phone_book)) {
+    return nonDuplicatedPrefix(phone_book.reverse());
+  } else {
+    return false;
+  }
 }
 
 console.log(solution(["119", "97674223", "1195524421"])); //false
