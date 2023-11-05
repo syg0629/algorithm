e24babbcd444cc0b6b29a134ebe6a44f5f76c938
syg0629 <syg0629@hanmail.net>
2023-11-05 20:03:49 +0900

    코드 리팩토링(sort메서드 사용하지 않은 풀이)

--- programmers/lv.2/231028-전화번호목록.ts
+++ programmers/lv.2/231028-전화번호목록.ts
@@ -1,27 +1,20 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/42577
 export {};
 
-function nonDuplicatedPrefix(phoneBook: string[]): boolean {
+function solution(phone_book: string[]) {
   const phoneNum: Record<string, boolean> = {};
 
-  for (const x of phoneBook) {
+  for (const x of phone_book) {
     phoneNum[x] = true;
+  }
+
+  for (const x of phone_book) {
     for (let i = 1; i < x.length; i++) {
       const cutPhoneNum: string = x.slice(0, i);
-      if (phoneNum[cutPhoneNum]) {
-        return false;
-      }
+      if (phoneNum[cutPhoneNum]) return false;
     }
   }
   return true;
-}
-
-function solution(phone_book: string[]): boolean {
-  if (nonDuplicatedPrefix(phone_book)) {
-    return nonDuplicatedPrefix(phone_book.reverse());
-  } else {
-    return false;
-  }
 }
 
 console.log(solution(["119", "97674223", "1195524421"])); //false
