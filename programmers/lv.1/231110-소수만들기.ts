2cf19b2805fd9ef6016c2bb79bdf51e9a460e136
syg0629 <syg0629@hanmail.net>
2023-11-12 19:48:33 +0900

    새로푼 문제 3개(23.11.10)

---
+++ programmers/lv.1/231110-소수만들기.ts
@@ -0,0 +1,24 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12977
+export {};
+
+function solution(nums: number[]): number {
+  let answer: number = 0;
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let e = j + 1; e < nums.length; e++) {
+        if (isPrime(nums[i] + nums[j] + nums[e])) answer++;
+      }
+    }
+  }
+  return answer;
+}
+
+function isPrime(n: number): boolean {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(solution([1, 2, 3, 4])); //1
+console.log(solution([1, 2, 7, 6, 4])); //4
