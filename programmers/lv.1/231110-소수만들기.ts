0f694f9ce80248f3fd03398982bdb26eae41da6d
syg0629 <syg0629@hanmail.net>
2023-11-21 19:26:17 +0900

    refactor: 문제 조건 바꿔서 풀기

--- programmers/lv.1/231110-소수만들기.ts
+++ programmers/lv.1/231110-소수만들기.ts
@@ -1,16 +1,23 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12977
 export {};
 
-function solution(nums: number[]): number {
+function solution(nums: number[], numCnt: number): number {
   let answer: number = 0;
-  const offset = 1;
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + offset; j < nums.length; j++) {
-      for (let e = j + offset; e < nums.length; e++) {
-        if (isPrime(nums[i] + nums[j] + nums[e])) answer++;
+
+  function findConbinations(idx: number, com: number[], numCnt: number) {
+    if (com.length === numCnt) {
+      const sum = com.reduce((x: number, y: number) => x + y, 0);
+      if (isPrime(sum)) answer++;
+      return;
+    } else {
+      for (let i = idx; i < nums.length; i++) {
+        com.push(nums[i]);
+        findConbinations(i + 1, com, numCnt);
+        com.pop();
       }
     }
   }
+  findConbinations(0, [], numCnt);
   return answer;
 }
 
@@ -21,5 +28,5 @@ function isPrime(n: number): boolean {
   return true;
 }
 
-console.log(solution([1, 2, 3, 4])); //1
-console.log(solution([1, 2, 7, 6, 4])); //4
+console.log(solution([1, 2, 3, 4], 3)); //1
+console.log(solution([1, 2, 7, 6, 4], 3)); //4
