ba727271ee950617e09669b676e1f61678af9b63
syg0629 <syg0629@hanmail.net>
2023-11-23 22:57:53 +0900

    refactor: 스택으로 풀기

--- programmers/lv.1/231110-소수만들기.ts
+++ programmers/lv.1/231110-소수만들기.ts
@@ -1,23 +1,34 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12977
+/**
+주어진 숫자 중 N개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다. 숫자들이 들어있는 배열 nums, 더하는 숫자의 개수 N이 매개변수로 주어질 때, nums에 있는 숫자들 중 서로 다른 N개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.(스택으로 풀기)
+ */
 export {};
 
-function solution(nums: number[], numCnt: number): number {
+function solution(nums: number[], N: number): number {
   let answer: number = 0;
+  const stack: number[] = [];
+  let idx: number = 0;
 
-  function findConbinations(idx: number, com: number[], numCnt: number) {
-    if (com.length === numCnt) {
-      const sum = com.reduce((x: number, y: number) => x + y, 0);
+  while (true) {
+    if (stack.length === N) {
+      const sum: number = stack.reduce((x: number, y: number) => x + y, 0);
       if (isPrime(sum)) answer++;
-      return;
+    }
+
+    if (idx === nums.length && stack.length === 0) {
+      break;
+    }
+
+    if (idx < nums.length) {
+      stack.push(nums[idx]);
     } else {
-      for (let i = idx; i < nums.length; i++) {
-        com.push(nums[i]);
-        findConbinations(i + 1, com, numCnt);
-        com.pop();
+      const popedItem = stack.pop();
+      for (const [i, x] of nums.entries()) {
+        if (x === popedItem) idx = i;
       }
     }
+    idx++;
   }
-  findConbinations(0, [], numCnt);
   return answer;
 }
 
