03a18bb069f813f12b3c9aa2c20d585b32a498c5
syg0629 <syg0629@hanmail.net>
2023-12-22 19:47:46 +0900

    refactor: count 변수 사용 및 i값을 변경

--- inflearn_js/section5 (투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/최대매출.ts
+++ inflearn_js/section5 (투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/최대매출.ts
@@ -3,18 +3,41 @@
  */
 export {};
 
+// function solution(k: number, arr: number[]): number {
+//   let sum: number = 0;
+//   let answer: number = 0;
+
+//   for (let i = 0; i < k; i++) {
+//     sum += arr[i];
+//     answer = sum;
+//   }
+
+//   for (let i = k; i < arr.length; i++) {
+//     sum += arr[i] - arr[i - k];
+//     answer = Math.max(answer, sum);
+//   }
+//   return answer;
+// }
+
 function solution(k: number, arr: number[]): number {
-  let answer: number = 0;
   let sum: number = 0;
-  for (let i = 0; i < k; i++) {
-    sum += arr[i];
-    answer = sum;
+  let count: number = 0;
+  let max: number = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (count < k) {
+      sum += arr[i];
+      count++;
+    } else {
+      if (max < sum) {
+        max = sum;
+      }
+      i = i - count;
+      count = 0;
+      sum = 0;
+    }
   }
-  for (let i = k; i < arr.length; i++) {
-    sum += arr[i] - arr[i - k];
-    answer = Math.max(answer, sum);
-  }
-  return answer;
+  return max;
 }
 
 console.log(solution(3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15])); //56
