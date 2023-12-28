1a0333fe6b20416b1a917f4a97ee465076e7cc12
syg0629 <syg0629@hanmail.net>
2023-12-28 23:59:41 +0900

    section6 문제풀이

---
+++ inflearn_js/section6(자료구조 - 스택, 큐)/후위식연산(postfix).ts
@@ -0,0 +1,26 @@
+/**
+ * 후위연산식이 주어지면 연산한 결과를 출력
+ */
+export {};
+
+function solution(s: string): number {
+  let answer: number = 0;
+  const operator: number[] = [];
+  for (const x of s) {
+    if (!isNaN(x)) operator.push(Number(x));
+    else {
+      let rt: number | undefined = operator.pop();
+      let lt: number | undefined = operator.pop();
+      if (rt !== undefined && lt !== undefined) {
+        if (x === "+") operator.push(lt + rt);
+        else if (x === "-") operator.push(lt - rt);
+        else if (x === "*") operator.push(lt * rt);
+        else if (x === "/") operator.push(lt / rt);
+      }
+    }
+  }
+  answer = operator[0];
+  return answer;
+}
+
+console.log(solution("352+*9-")); //12
