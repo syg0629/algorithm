b3e98ff6408ae0ea09480a7796354d12cc2e2923
syg0629 <syg0629@hanmail.net>
2024-01-22 11:59:58 +0900

    새로푼 문제 추가

---
+++ programmers/lv.1/240122-[1차]비밀지도.ts
@@ -0,0 +1,16 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/17681
+export {};
+
+function solution(n: number, arr1: number[], arr2: number[]): string[] {
+  const combineMap: string[] = arr1.map((x, i) =>
+    (x | arr2[i]).toString(2).padStart(n, "0")
+  );
+  const decoding: string[] = combineMap.map((x) =>
+    x.replace(/1/g, "#").replace(/0/g, " ")
+  );
+  return decoding;
+}
+
+console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
+//["#####","# # #", "### #", "# ##", "#####"]
+console.log(solution(6, [46, 33, 33, 22, 31, 50], [27, 56, 19, 14, 14, 10])); //["######", "### #", "## ##", " #### ", " #####", "### # "]
