56ffe6baed2233cc6306191edf7bbaf9073d82ec
syg0629 <syg0629@hanmail.net>
2024-01-22 15:43:19 +0900

    새로푼 문제 추가

---
+++ programmers/lv.1/240122-명예의전당.ts
@@ -0,0 +1,26 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/138477
+export {};
+
+function solution(k: number, score: number[]): number[] {
+  const orderedScores: number[] = [];
+  const answer: number[] = [];
+
+  for (const x of score) {
+    orderedScores.push(x);
+    orderedScores.sort((a: number, b: number) => b - a);
+
+    if (orderedScores.length >= k) {
+      answer.push(orderedScores[k - 1]);
+    } else {
+      const lastNum: number | undefined = orderedScores.at(-1);
+      if (lastNum) {
+        answer.push(lastNum);
+      }
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(3, [10, 100, 20, 150, 1, 100, 200])); //[10, 10, 10, 20, 20, 100, 100]
+console.log(solution(4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000])); //[0, 0, 0, 0, 20, 40, 70, 70, 150, 300]
