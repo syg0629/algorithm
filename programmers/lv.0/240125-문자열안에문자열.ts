ff3ddd2f10c11fe35adc59f0059e46e603017cd3
syg0629 <syg0629@hanmail.net>
2024-01-26 22:27:59 +0900

    refactor: 용도에 맞는 함수로 수정

--- programmers/lv.0/240125-문자열안에문자열.ts
+++ programmers/lv.0/240125-문자열안에문자열.ts
@@ -1,8 +1,9 @@
-//
+//https://school.programmers.co.kr/learn/courses/30/lessons/120908
 export {};
+
 function solution(str1: string, str2: string): number {
-  const isStr2InStr1 = str1.split(str2);
-  return isStr2InStr1.length === 1 ? 2 : 1;
+  const isStr2InStr1: boolean = str1.includes(str2);
+  return isStr2InStr1 ? 1 : 2;
 }
 
 console.log(solution("ab6CDE443fgh22iJKlmn1o", "6CD")); //1
