387a297e5e1199cba6fa60ebdfc0e0046e6cd51c
syg0629 <syg0629@hanmail.net>
2024-01-26 22:28:42 +0900

    refactor: 반복문 횟수 줄이기

--- programmers/lv.0/240125-짝수의합.ts
+++ programmers/lv.0/240125-짝수의합.ts
@@ -3,7 +3,7 @@ export {};
 
 function solution(n: number): number {
   let sum: number = 0;
-  for (let i = 0; i <= n; i++) {
+  for (let i = 2; i <= n; i += 2) {
     if (i % 2 === 0) {
       sum += i;
     }
