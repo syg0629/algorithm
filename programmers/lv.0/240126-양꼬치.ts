4f7893ffa9b6840702e0bac3f5f9b4cba667099a
syg0629 <syg0629@hanmail.net>
2024-01-26 22:30:12 +0900

    refactor: 코드 간결하게 수정

--- programmers/lv.0/240126-양꼬치.ts
+++ programmers/lv.0/240126-양꼬치.ts
@@ -2,13 +2,7 @@
 export {};
 
 function solution(n: number, k: number): number {
-  let free = 0;
-  if (n >= 10) {
-    free = Math.floor(n / 10);
-    return n * 12000 + (k - free) * 2000;
-  } else {
-    return n * 12000 + k * 2000;
-  }
+  return n * 12000 + (k - Math.floor(n / 10)) * 2000;
 }
 
 console.log(solution(10, 3)); //124,000
