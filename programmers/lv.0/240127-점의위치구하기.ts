7b5a00e10ab4ec43ca6ebcea61251ea37203ad99
syg0629 <syg0629@hanmail.net>
2024-01-28 16:32:54 +0900

    refactor: 구조 분해 할당으로 수정, 타입 개선

--- programmers/lv.0/240127-점의위치구하기.ts
+++ programmers/lv.0/240127-점의위치구하기.ts
@@ -1,16 +1,15 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/120841
 export {};
 
-function solution(dot: number[]): number | undefined {
-  if (dot.length >= 2 && dot[0] !== undefined && dot[1] !== undefined) {
-    if (dot[0] > 0 && dot[1] > 0) {
-      return 1;
-    } else if (dot[0] < 0 && dot[1] > 0) {
-      return 2;
-    } else if (dot[0] < 0 && dot[1] < 0) {
-      return 3;
-    } else return 4;
-  }
+function solution(dot: number[]): number {
+  const [num1, num2] = dot;
+  if (num1 > 0 && num2 > 0) {
+    return 1;
+  } else if (num1 < 0 && num2 > 0) {
+    return 2;
+  } else if (num1 < 0 && num2 < 0) {
+    return 3;
+  } else return 4;
 }
 
 console.log(solution([2, 4])); //1
