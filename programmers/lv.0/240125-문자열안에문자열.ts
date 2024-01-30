f66db1a211933d52e95939595e0fcda9776510b1
syg0629 <syg0629@hanmail.net>
2024-01-30 19:17:34 +0900

    refactor: 코드 간결하게 수정하기

--- programmers/lv.0/240125-문자열안에문자열.ts
+++ programmers/lv.0/240125-문자열안에문자열.ts
@@ -2,8 +2,7 @@
 export {};
 
 function solution(str1: string, str2: string): number {
-  const isStr2InStr1: boolean = str1.includes(str2);
-  return isStr2InStr1 ? 1 : 2;
+  return str1.includes(str2) ? 1 : 2;
 }
 
 console.log(solution("ab6CDE443fgh22iJKlmn1o", "6CD")); //1
