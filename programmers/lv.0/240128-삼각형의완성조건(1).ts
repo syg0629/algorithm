6218d7a732f9cb6e0791a3507629fca1214d8b79
syg0629 <syg0629@hanmail.net>
2024-01-31 09:29:56 +0900

    refactor: 매개변수 타입 수정

--- programmers/lv.0/240128-삼각형의완성조건(1).ts
+++ programmers/lv.0/240128-삼각형의완성조건(1).ts
@@ -1,7 +1,7 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/120889
 export {};
 
-function solution(sides: number[]): number {
+function solution(sides: [number, number, number]): number {
   sides.sort((a, b) => b - a);
   return sides[0] < sides[1] + sides[2] ? 1 : 2;
 }
