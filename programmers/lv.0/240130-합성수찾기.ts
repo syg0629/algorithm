0a2ba9574f811bdbd6eca7c46a16425ca7986dc1
syg0629 <syg0629@hanmail.net>
2024-01-31 10:57:21 +0900

    refactor: isPrime으로 함수 변경

--- programmers/lv.0/240130-합성수찾기.ts
+++ programmers/lv.0/240130-합성수찾기.ts
@@ -4,20 +4,20 @@ export {};
 function solution(n: number): number {
   let cnt: number = 0;
   for (let i = 1; i <= n; i++) {
-    if (isNotPrime(i)) {
+    if (isPrime(i)) {
       cnt++;
     }
   }
-  return cnt;
+  return n - cnt;
 }
 
-function isNotPrime(num: number): boolean {
+function isPrime(num: number): boolean {
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
-      return true;
+      return false;
     }
   }
-  return false;
+  return true;
 }
 
 console.log(solution(10)); //5
