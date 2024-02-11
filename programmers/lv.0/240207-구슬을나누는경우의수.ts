a3e417a0ed624c133d924e3a4020688af34a141f
syg0629 <syg0629@hanmail.net>
2024-02-11 23:31:39 +0900

    refactor: shareF쓰지 않고 풀기

--- programmers/lv.0/240207-구슬을나누는경우의수.ts
+++ programmers/lv.0/240207-구슬을나누는경우의수.ts
@@ -7,12 +7,12 @@ function solution(balls: number, share: number): number {
     ballsF *= i;
   }
 
-  let shareF: number = 1;
-  for (let i = share; i >= 1; i--) {
-    shareF *= i;
+  for (let i = 1; i <= share; i++) {
+    ballsF /= i;
   }
-  return Math.floor(ballsF / shareF);
-}
 
+  return Math.round(ballsF);
+}
 console.log(solution(3, 2)); //3
 console.log(solution(5, 3)); //10
+console.log(solution(30, 25)); //142506 Math.round안 붙이면 142506.00000000006
