0599e1bd61e32a5f6c407e96e3ad6624559e7df0
syg0629 <syg0629@hanmail.net>
2024-02-13 14:02:12 +0900

    refactor: parseInt 없이 풀이

--- programmers/lv.0/240203-이진수더하기.ts
+++ programmers/lv.0/240203-이진수더하기.ts
@@ -2,8 +2,18 @@
 export {};
 
 function solution(bin1: string, bin2: string): string {
-  return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
+  return (binaryToDecimal(bin1) + binaryToDecimal(bin2)).toString(2);
 }
+
+const binaryToDecimal = (str: string): number => {
+  const decimalNum = str
+    .split("")
+    .reverse()
+    .map(Number)
+    .map((x, y) => x * Math.pow(2, y))
+    .reduce((x, y) => x + y);
+  return decimalNum;
+};
 
 console.log(solution("10", "11")); //"101"
 console.log(solution("1001", "1111")); //"11000"
