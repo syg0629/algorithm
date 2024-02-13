c8d9d407d62492a626a55585f0932a5dab59fa98
syg0629 <syg0629@hanmail.net>
2024-02-13 13:30:17 +0900

    refactor: 코드 개선

--- programmers/lv.0/240208-삼각형의완성조건(2).ts
+++ programmers/lv.0/240208-삼각형의완성조건(2).ts
@@ -2,13 +2,7 @@
 export {};
 
 function solution(sides: number[]): number {
-  const max: number = Math.max(...sides);
-  const min: number = Math.min(...sides);
-  let cnt: number = 0;
-  for (let i = max - min + 1; i < max + min; i++) {
-    cnt++;
-  }
-  return cnt;
+  return Math.min(...sides) * 2 - 1;
 }
 
 console.log(solution([1, 2])); //1
