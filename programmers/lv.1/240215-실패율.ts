814ae1732c81a664bb599c9e920c465b1f378546
syg0629 <syg0629@hanmail.net>
2024-02-16 13:26:16 +0900

    refactor: 타입 개선

--- programmers/lv.1/240215-실패율.ts
+++ programmers/lv.1/240215-실패율.ts
@@ -5,7 +5,7 @@ export {};
 //전체 스테이지의 개수 N, 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages
 function solution(N: number, stages: number[]): number[] {
   let answer: number[] = [];
-  const failureLates: number[][] = [];
+  const failureLates: [number, number][] = [];
   for (let i = 1; i <= N; i++) {
     const reach = stages.filter((x) => x >= i).length;
     const clear = stages.filter((x) => x === i).length;
