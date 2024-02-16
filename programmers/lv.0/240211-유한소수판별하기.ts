6158c1d0e66799042f70f5f76fa0d0fd52443b71
syg0629 <syg0629@hanmail.net>
2024-02-16 14:15:37 +0900

    refactor: 최대공약수 재귀 함수로 만들기

--- programmers/lv.0/240211-유한소수판별하기.ts
+++ programmers/lv.0/240211-유한소수판별하기.ts
@@ -2,21 +2,21 @@
 export {};
 
 function solution(a: number, b: number): number {
-  //최대공약수
-  let gcd: number = 1;
-  for (let i = 1; i <= Math.min(a, b); i++) {
-    if (a % i === 0 && b % i === 0) {
-      gcd = i;
-    }
-  }
+  b /= gcd(a, b);
 
-  b /= gcd;
-
-  while (b % 2 === 0) b = b / 2;
-  while (b % 5 === 0) b = b / 5;
+  while (b % 2 === 0) b /= 2;
+  while (b % 5 === 0) b /= 5;
 
   return b === 1 ? 1 : 2;
 }
+
+//최대공약수
+const gcd = (a: number, b: number): number => {
+  if (b === 0) {
+    return a;
+  }
+  return gcd(b, a % b);
+};
 
 console.log(solution(7, 20)); //1
 console.log(solution(11, 22)); //1
