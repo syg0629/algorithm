a74d8ed6a462523a5d67de18d4b59814cce968d1
syg0629 <syg0629@hanmail.net>
2024-02-23 08:51:23 +0900

    refactor: 코드 개선

--- programmers/lv.0/240210-직사각형넓이구하기.ts
+++ programmers/lv.0/240210-직사각형넓이구하기.ts
@@ -1,9 +1,14 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/120860
 export {};
 
-function solution(dots: number[][]): number {
-  dots.sort((x: number[], y: number[]) => x[0] - y[0]);
-  return Math.abs(dots[0][0] - dots[2][0]) * Math.abs(dots[0][1] - dots[1][1]);
+function solution(dots: [number, number][]): number {
+  const x: number[] = [];
+  const y: number[] = [];
+  for (const z of dots) {
+    x.push(z[0]);
+    y.push(z[1]);
+  }
+  return (Math.max(...x) - Math.min(...x)) * (Math.max(...y) - Math.min(...y));
 }
 
 //prettier-ignore
