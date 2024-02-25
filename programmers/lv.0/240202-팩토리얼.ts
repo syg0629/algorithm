7b7b67dfe21fadf356fad5d78624fde30028806a
syg0629 <syg0629@hanmail.net>
2024-02-25 15:08:18 +0900

    refactor: 주석 삭제

--- programmers/lv.0/240202-팩토리얼.ts
+++ programmers/lv.0/240202-팩토리얼.ts
@@ -1,18 +1,6 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/120848
 export {};
 
-// function solution(n: number): number {
-//   return factorial(n, 1, 1);
-// }
-
-// function factorial(n: number, answer: number, curFactorial: number): number {
-//   curFactorial *= answer;
-//   if (curFactorial > n) {
-//     return answer - 1;
-//   }
-//   return factorial(n, answer + 1, curFactorial);
-// }
-
 function solution(n: number): number {
   return factorial(n, 1);
 }
