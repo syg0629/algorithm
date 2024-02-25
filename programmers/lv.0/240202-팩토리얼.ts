47fe82df8b030307acb79e0221e7c271504ba472
syg0629 <syg0629@hanmail.net>
2024-02-25 15:00:31 +0900

    refactor: 매개변수 삭제

--- programmers/lv.0/240202-팩토리얼.ts
+++ programmers/lv.0/240202-팩토리얼.ts
@@ -1,16 +1,27 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/120848
 export {};
 
+// function solution(n: number): number {
+//   return factorial(n, 1, 1);
+// }
+
+// function factorial(n: number, answer: number, curFactorial: number): number {
+//   curFactorial *= answer;
+//   if (curFactorial > n) {
+//     return answer - 1;
+//   }
+//   return factorial(n, answer + 1, curFactorial);
+// }
+
 function solution(n: number): number {
-  return factorial(n, 1, 1);
+  return factorial(n, 1);
 }
 
-function factorial(n: number, answer: number, curFactorial: number): number {
-  curFactorial *= answer;
-  if (curFactorial > n) {
+function factorial(n: number, answer: number): number {
+  if (answer > n) {
     return answer - 1;
   }
-  return factorial(n, answer + 1, curFactorial);
+  return factorial(n / answer, answer + 1);
 }
 
 console.log(solution(3628800)); //10
