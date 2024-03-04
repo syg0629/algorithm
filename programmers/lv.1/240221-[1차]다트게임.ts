8459c031c06ada8375dc378330609a1f2031ee9f
syg0629 <syg0629@hanmail.net>
2024-03-04 11:08:59 +0900

    refactor: 코드 개선

--- programmers/lv.1/240221-[1차]다트게임.ts
+++ programmers/lv.1/240221-[1차]다트게임.ts
@@ -14,14 +14,14 @@ function solution(dartResult: string): number {
         num = "";
       }
 
+      const poppedScore = scores.pop()!;
       if (x === "*") {
-        const prev = scores.pop()!;
-        scores.push((scores.pop()! || 0) * 2);
-        scores.push(prev * 2);
+        const prevScore = scores.pop()! || 0;
+        scores.push(prevScore * 2, poppedScore * 2);
       } else if (x === "#") {
-        scores.push(scores.pop()! * -1);
+        scores.push(poppedScore * -1);
       } else {
-        scores.push(calculateScore(scores.pop()!, x)!);
+        scores.push(calculateScore(poppedScore, x)!);
       }
     }
   }
@@ -29,10 +29,8 @@ function solution(dartResult: string): number {
 }
 
 function calculateScore(score: number, bonus: string): number {
-  if (bonus === "S") return Math.pow(score, 1);
-  if (bonus === "D") return Math.pow(score, 2);
-  if (bonus === "T") return Math.pow(score, 3);
-  return 0;
+  const bonusScoreTable: Record<string, number> = { S: 1, D: 2, T: 3 };
+  return Math.pow(score, bonusScoreTable[bonus]);
 }
 
 console.log(solution("1S2D*3T")); //37
@@ -42,3 +40,4 @@ console.log(solution("1S*2T*3S")); //23
 console.log(solution("1D#2S*3S")); //5
 console.log(solution("1T2D3D#")); //-4
 console.log(solution("1D2S3T*")); //59
+console.log(solution("1S2D3T*")); //63
