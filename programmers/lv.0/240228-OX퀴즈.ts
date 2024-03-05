f537ec6c9b5a825777d6cfe8121738bc3cea6164
syg0629 <syg0629@hanmail.net>
2024-03-05 11:14:27 +0900

    refactor: 중복 개선

--- programmers/lv.0/240228-OX퀴즈.ts
+++ programmers/lv.0/240228-OX퀴즈.ts
@@ -4,13 +4,19 @@ export {};
 function solution(quiz: string[]): string[] {
   const answer: string[] = quiz.map((a: string) => {
     const [x, operator, y, _, result] = a.split(" ");
-    if (operator === "+") {
-      return Number(x) + Number(y) === Number(result) ? "O" : "X";
-    } else {
-      return Number(x) - Number(y) === Number(result) ? "O" : "X";
-    }
+    return calculate(Number(x), Number(y), operator) === Number(result)
+      ? "O"
+      : "X";
   });
   return answer;
+}
+
+function calculate(x: number, y: number, operator: string): number {
+  if (operator === "+") {
+    return x + y;
+  } else {
+    return x - y;
+  }
 }
 
 console.log(solution(["3 - 4 = -3", "5 + 6 = 11"])); //["X", "O"]
