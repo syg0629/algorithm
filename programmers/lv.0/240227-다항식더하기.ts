327afb5b759c99a13de241a6c3aca8f311673fe8
syg0629 <syg0629@hanmail.net>
2024-03-05 10:55:29 +0900

    refactor: 코드 개선

--- programmers/lv.0/240227-다항식더하기.ts
+++ programmers/lv.0/240227-다항식더하기.ts
@@ -7,18 +7,19 @@ function solution(polynomial: string): string {
   const terms: string[] = polynomial.split(" + ");
   for (const x of terms) {
     if (x.includes("x")) {
-      let xTermsNum: string = x.replace("x", "") || "1";
-      xTermsSum += Number(xTermsNum);
+      let xTermsNum: number = Number(x.split("x")[0]) || 1;
+      xTermsSum += xTermsNum;
     } else {
       constantTermsSum += Number(x);
     }
   }
-  const answer: string[] = [];
-  if (xTermsSum) answer.push(`${xTermsSum === 1 ? "" : xTermsSum}x`);
-  if (constantTermsSum) answer.push(constantTermsSum.toString());
-  return answer.join(" + ");
+  let answer: string = "";
+  if (xTermsSum) answer += `${xTermsSum === 1 ? "" : xTermsSum}x`;
+  if (constantTermsSum)
+    answer += (xTermsSum ? " + " : "") + constantTermsSum.toString();
+  return answer;
 }
 
 console.log(solution("3x + 7 + x")); //"4x + 7"
 console.log(solution("x + x + x")); //"3x"
-console.log(solution("x + 1")); //"3x"
+console.log(solution("x + 1")); //"x+1"
