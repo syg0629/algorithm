8d93e900191ecf17be1e4f4c5d3cf9cf39d06d85
syg0629 <syg0629@hanmail.net>
2024-03-29 13:16:34 +0900

    Refactor: lodash range로 변경

--- programmers/lv.0/240315-연속된수의합.ts
+++ programmers/lv.0/240315-연속된수의합.ts
@@ -1,14 +1,11 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/120923
+import _ from "lodash";
 export {};
 
 function solution(num: number, total: number): number[] {
   const avg: number = Math.ceil(total / num);
   const startNum: number = avg - Math.trunc(num / 2);
-  const answer: number[] = [];
-  for (let i = 0; i < num; i++) {
-    answer.push(startNum + i);
-  }
-  return answer;
+  return _.range(startNum, startNum + num, 1);
 }
 
 console.log(solution(3, 12)); //[3, 4, 5]
