c8219856740b78f9fe95e8427dbd1e444e3c2212
syg0629 <syg0629@hanmail.net>
2024-04-08 18:05:30 +0900

    Refactor: 시간복잡도 개선

--- programmers/lv.1/240215-실패율.ts
+++ programmers/lv.1/240215-실패율.ts
@@ -4,17 +4,22 @@ export {};
 //실패율 : 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
 //전체 스테이지의 개수 N, 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages
 function solution(N: number, stages: number[]): number[] {
-  let answer: number[] = [];
-  const failureLates: [number, number][] = [];
-  for (let i = 1; i <= N; i++) {
-    const reach = stages.filter((x) => x >= i).length;
-    const clear = stages.filter((x) => x === i).length;
-    failureLates.push([i, clear / reach]);
+  const playersStage: Map<number, number> = new Map();
+  const failureRates: [number, number][] = [];
+  let reachedPlayers: number = stages.length;
+
+  for (const x of stages) {
+    playersStage.set(x, (playersStage.get(x) ?? 0) + 1);
   }
-  answer = failureLates
+  for (let i = 1; i <= N; i++) {
+    const unsolvedPlayers: number = playersStage.get(i) ?? 0;
+    failureRates.push([i, unsolvedPlayers / reachedPlayers]);
+    reachedPlayers -= unsolvedPlayers;
+  }
+
+  return failureRates
     .sort((a: number[], b: number[]) => b[1] - a[1])
     .map((x) => x[0]);
-  return answer;
 }
 
 console.log(solution(5, [2, 1, 2, 6, 2, 4, 3, 3])); //[3,4,2,1,5]
