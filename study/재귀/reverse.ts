8181decf5002e66313bd7d897a2478615394173f
syg0629 <syg0629@hanmail.net>
2024-05-03 23:44:16 +0900

    Refactor: 코드 리팩토링

--- study/재귀/reverse.ts
+++ study/재귀/reverse.ts
@@ -1,14 +1,8 @@
 // 문자열을 받아들이고 그 문자열의 역순인 문자열을 반환하는 함수 작성
 
 function reverse(str: string): string {
-  let reversedStr = "";
-  function reverseFn(str: string): string {
-    if (str.length === 0) return "";
-    reversedStr += str[str.length - 1];
-    return reverseFn(str.slice(0, str.length - 1));
-  }
-  reverseFn(str);
-  return reversedStr;
+  if (str.length <= 1) return str;
+  return reverse(str.slice(1)) + str[0];
 }
 
 console.log(reverse("awesome")); // 'emosewa'
