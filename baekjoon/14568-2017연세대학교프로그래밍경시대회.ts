cef6d1438108de5ce86b739a83c95e906f3a6ef7
syg0629 <syg0629@hanmail.net>
2024-07-23 10:33:29 +0900

    Refactor: 링크 삭제

--- baekjoon/14568-2017연세대학교프로그래밍경시대회.ts
+++ baekjoon/14568-2017연세대학교프로그래밍경시대회.ts
@@ -1,5 +1,4 @@
 //https://www.acmicpc.net/problem/14568
-//youtu.be/ctD280A0iM4?si=hi9ZARLTKpzSDWdM&t=1068
 export {};
 
 const fs = require("fs");
@@ -9,11 +8,13 @@ const input = fs.readFileSync(filePath).toString().trim();
 const candy = Number(input);
 
 let count = 0;
-for (let t = 2; t < candy; t += 2) {
-  for (let y = 1; y < candy - t; y++) {
-    const n = candy - t - y;
-    if (n >= y + 2) {
-      count++;
+for (let i = candy; i > 0; i--) {
+  for (let t = 2; t < candy; t += 2) {
+    for (let y = 1; y < candy - t; y++) {
+      const n = candy - t - y;
+      if (n >= y + 2) {
+        count++;
+      }
     }
   }
 }
