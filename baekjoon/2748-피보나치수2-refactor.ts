7039d3dcc27f94e88b73fe6427301fc20f11d5a9
syg0629 <syg0629@hanmail.net>
2024-10-16 08:53:52 +0900

    refactor: JS 숫자 한계로 BigInt 사용

--- baekjoon/2748-피보나치수2-refactor.ts
+++ baekjoon/2748-피보나치수2-refactor.ts
@@ -6,13 +6,13 @@ const filePath =
   process.platform === "linux" ? "/dev/stdin" : __dirname + "/input1.txt";
 const input = Number(fs.readFileSync(filePath).toString().trim());
 
-const fib = (n: number, memo: { [key: number]: number } = {}): number => {
-  if (n === 0) return 0;
-  if (n <= 2) return 1;
+const fib = (n: number, memo: { [key: number]: bigint } = {}): bigint => {
+  if (n === 0) return BigInt(0);
+  if (n <= 2) return BigInt(1);
   if (memo[n]) return memo[n];
 
   memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
   return memo[n];
 };
 
-console.log(fib(input));
+console.log(fib(input).toString());
