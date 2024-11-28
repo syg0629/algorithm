2f55d68c1c5a150c3487547dce3ae09febf0de04
syg0629 <syg0629@hanmail.net>
2024-11-28 23:55:42 +0900

    241128

---
+++ baekjoon/11651-좌표정렬하기2.ts
@@ -0,0 +1,17 @@
+//https://www.acmicpc.net/problem/11651
+export {};
+
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/input2.txt";
+const [_, ...input] = fs.readFileSync(filePath).toString().trim().split("\n");
+const coordinate = input.map((x: string) => x.split(" ").map(Number));
+const sorted = coordinate.sort((a: number[], b: number[]) => {
+  if (a[1] !== b[1]) {
+    return a[1] - b[1];
+  } else {
+    return a[0] - b[0];
+  }
+});
+
+console.log(sorted.join("\n").replaceAll(",", " "));
