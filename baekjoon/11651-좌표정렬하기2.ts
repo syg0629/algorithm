ef2f86a0bcc030e5060ced6264ad266a325b8d58
syg0629 <syg0629@hanmail.net>
2024-11-29 10:28:50 +0900

    Refactor: 더 효율적으로 수정

--- baekjoon/11651-좌표정렬하기2.ts
+++ baekjoon/11651-좌표정렬하기2.ts
@@ -6,12 +6,11 @@ const filePath =
   process.platform === "linux" ? "/dev/stdin" : __dirname + "/input2.txt";
 const [_, ...input] = fs.readFileSync(filePath).toString().trim().split("\n");
 const coordinate = input.map((x: string) => x.split(" ").map(Number));
-const sorted = coordinate.sort((a: number[], b: number[]) => {
+coordinate.sort((a: number[], b: number[]) => {
   if (a[1] !== b[1]) {
     return a[1] - b[1];
   } else {
     return a[0] - b[0];
   }
 });
-
-console.log(sorted.join("\n").replaceAll(",", " "));
+console.log(coordinate.map((x: number[]) => x.join(" ")).join("\n"));
