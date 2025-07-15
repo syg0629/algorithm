382ced8e35b44cae4ee747016d463a22e4332a62
syg0629 <syg0629@hanmail.net>
2025-07-16 01:47:43 +0900

    250716

---
+++ codility/PassingCars.js
@@ -0,0 +1,15 @@
+function solution(A) {
+  let eastCars = 0;
+  let passingPairs = 0;
+
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] === 0) {
+      eastCars++;
+    } else {
+      passingPairs += eastCars;
+    }
+  }
+  return passingPairs;
+}
+
+console.log(solution([0, 1, 0, 1, 1])); //5
